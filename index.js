28d979c1deb860fe00b537cff4e2cf95b9781e7a
BigGunBrother <nokelove1111@gmail.com>
2024-08-29 18:58:32 +0800

    fix: 修改routes 路由

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const todoRoutes = require("./router/todoRoutes");
+const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 
